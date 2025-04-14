68a6c8643802035d18666e00004e9ed176d49b27
RanjitaRevankar <revankarranjita215@gmail.com>
2025-04-14 11:26:42 +0530

    Add files via upload

---
+++ springboot-crudmysqldemo (1)/src/main/java/com/demo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.demo.controller;
+
+import com.demo.model.User;
+import com.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return service.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+    }
+}
---
+++ springboot-crudmysqldemo (1)/src/main/java/com/demo/model/User.java
@@ -0,0 +1,14 @@
+package com.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Getters and Setters
+}
