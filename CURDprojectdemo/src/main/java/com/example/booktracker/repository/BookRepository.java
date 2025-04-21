8d882979c839afc252b88eeca3e7cb2d6dd584d6
RanjitaRevankar <revankarranjita215@gmail.com>
2025-04-21 17:47:38 +0530

    Delete CURDprojectdemo directory

--- CURDprojectdemo/src/main/java/com/example/booktracker/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.booktracker.repository;
-
-import com.example.booktracker.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
-
